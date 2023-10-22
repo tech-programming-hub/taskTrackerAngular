de4d521737258d94bfd15dd31a4644c9a3a6207e
Anup Verma <anup20verma@gmail.com>
2023-10-22 15:37:25 +0530

    code changes for task tracker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CommonModule } from '@angular/common';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    AddTaskComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+      CommonModule,
+      FontAwesomeModule,
+      HttpClientModule,
+      FormsModule
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/serverResponses/mockTasks.ts
@@ -0,0 +1,34 @@
+import { Task } from '../model/Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: '1 task',
+    day: '1 May',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: '2 task',
+    day: '2 May',
+    reminder: true
+  },
+  {
+    id: 3,
+    text: '3 task',
+    day: '3 May',
+    reminder: true
+  },
+  {
+    id: 4,
+    text: '4 task',
+    day: '4 May',
+    reminder: true
+  },
+  {
+    id: 5,
+    text: '5 task',
+    day: '5 May',
+    reminder: true
+  }
+]
